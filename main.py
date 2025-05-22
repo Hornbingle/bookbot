927c49926f7e744e9126970f5548a832f0c69ef3
Hornbingle <jacobhornstein@rocketmail.com>
2025-05-22 16:54:12 -0700

    Yay it works for any file in the directory

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 from stats import word_count
 from stats import letter_count
+from stats import neat_output
+import sys
 
-current_book = "books/frankenstein.txt"
+number_of_sys_arguments = len(sys.argv)
+
+def arguments_check():
+    if number_of_sys_arguments < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        pass
+
+arguments_check()
+
+current_book = sys.argv[1]
 
 def main():
-    num_words = word_count(current_book)
-    num_letters = letter_count(current_book)
-    print(f"{num_words} words found in the document")
-    print(num_letters)
-    """print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {current_book}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")"""
+    try:
+        num_words = word_count(current_book)
+        num_letters = letter_count(current_book)
+        neat_counts = neat_output(current_book)
+        """print(f"{num_words} words found in the document")
+        print(num_letters)"""
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {current_book}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        print(neat_counts)
+        print("============= END ===============")
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+"""def input_check():
+    try:
+        main()
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)"""
 
 main()
--- stats.py
+++ stats.py
@@ -5,16 +5,16 @@ def get_book_test(current_book):
         file_contents = f.read()
         return file_contents
     
-def word_count(book_string):
-    file_contents = get_book_test(book_string)
+def word_count(current_book):
+    file_contents = get_book_test(current_book)
     split_contents = file_contents.split()
     words_number = 0
     for words in split_contents:
         words_number += 1
     return words_number
 
-def letter_count(book_string):
-    file_contents = get_book_test(book_string)
+def letter_count(current_book):
+    file_contents = get_book_test(current_book)
     lower_contents = file_contents.lower()
     letter_dictionary = {}
     for letter in lower_contents:
@@ -27,3 +27,30 @@ def letter_count(book_string):
         else:
             letter_dictionary[letter] = 1
     return letter_dictionary
+
+def sort_criteria(unsorted_dictionary_list):
+    return unsorted_dictionary_list["count"]
+
+def list_of_dictionaries(current_book):
+    unsorted_dictionary = letter_count(current_book)
+    unsorted_dictionary_list = []
+    for key in unsorted_dictionary:
+        temp_dictionary = {}
+        temp_dictionary["character"] = key
+        temp_dictionary["count"] = unsorted_dictionary[key]
+        unsorted_dictionary_list.append(temp_dictionary)
+    unsorted_dictionary_list.sort(reverse=True, key=sort_criteria)
+    return unsorted_dictionary_list
+
+def neat_output(current_book):
+    sdl = list_of_dictionaries(current_book)
+    output_string = ""
+    for dictionaries in sdl:
+        temp_string = ""
+        temp_character = dictionaries["character"]
+        temp_count = dictionaries["count"]
+        temp_string = f"{temp_character}: {temp_count}\n"
+        output_string += temp_string
+    return output_string
+
+#neat_output(current_book)
